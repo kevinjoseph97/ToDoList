0e7e63a055e3b210c061d273108ca63df9d24bec
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-09-01 00:20:39 -0400

    set up props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList';
 
 function App() {
+  // this will keep track of what we type into input box 
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header> 
-        <h1>To Do List</h1>
+        <h1>To Do List </h1>
       </header>
-      <Form/>
+      <Form setInputText ={setInputText}/>
       <TodoList/>
     </div>
   );
